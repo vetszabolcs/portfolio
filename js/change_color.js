f4c03ddaa55dcab7f570803d1b622dd29f46227c
Szabolcs Vetesi <witens11@gmail.com>
2022-07-30 20:30:32 +0200

    replaced files

--- js/change_color.js
+++
@@ -1,7 +0,0 @@
-let element = document.getElementById("contact-link");
-let touch = document.getElementById("touch");
-
-element.addEventListener("click", function (e){
-  touch.style.color = "white";
-  setTimeout(() => {touch.style.color = "black";}, 250);
-});
